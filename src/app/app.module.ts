ad59c66e22cde54af722f0f68d4a0736d4888cd6
Francisco do Vale <francisco_do_vale@epam.com>
2020-03-06 15:56:57 +0100

    feat(state): ngrx initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { effects } from '@app-store/effects';
+import { reducers } from '@app-store/reducers';
+
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const BASE_MODULES = [
+  AppRoutingModule,
+  BrowserModule,
+  BrowserAnimationsModule
+];
+
+const NGRX_MODULES = [
+  StoreModule.forRoot(reducers),
+  EffectsModule.forRoot(effects),
+  !environment.production ? StoreDevtoolsModule.instrument() : []
+];
+
+const FEATURE_MODULES = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    ...BASE_MODULES,
+    ...NGRX_MODULES,
+    ...FEATURE_MODULES ],
   providers: [],
-  bootstrap: [AppComponent]
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/store/reducers.ts
@@ -0,0 +1,5 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+export interface AppState {}
+
+export const reducers: ActionReducerMap<AppState> = {};
